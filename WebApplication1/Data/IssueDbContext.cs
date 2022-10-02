65ee4f33a2034c1e9f5ace45acd0c9bb5a1a76d6
unverifiedchaos5017 <saifkhan501721@gmail.com>
2022-10-02 13:16:51 -0700

    first commit

---
+++ WebApplication1/Data/IssueDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class IssueDbContext : DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
